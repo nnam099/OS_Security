c88ceabc6401bbca385adbc1ee7c5cbcd3f5074c
nnam09 <khachhanhtltd663@gmail.com>
2026-03-22 22:15:58 +0700

    Create vulnerable-program-fix.c

---
+++ Race_Condition/vulnerable-program-fix.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#define DELAY 500000
+
+int main(int argc, char * argv[]) {
+    char * fileName = argv[1];
+    char buffer[60];
+    int i;
+    FILE * fileHandler;
+
+    scanf("%50s", buffer);
+    
+    if (!access(fileName, W_OK)) {
+        for (i = 0; i < DELAY; i++) {
+            int a = i ^ 2;
+        }
+        
+        seteuid(getuid());
+        
+        fileHandler = fopen(fileName, "a+");
+        fwrite("\n", sizeof(char), 1, fileHandler);
+        fwrite(buffer, sizeof(char), strlen(buffer), fileHandler);
+        fwrite("\n", sizeof(char), 1, fileHandler);
+        fclose(fileHandler);
+    }
+    else {
+        printf("No permission \n");
+    }
+}
